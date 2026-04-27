a59902e61a2852ee4f2cc6d2960bb2aa67b15e6a
jojoe260245 <jojoe260245@gmail.com>
2026-04-28 06:39:01 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "/ThungmakhamMiamiFood";
+
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "latasha-unrelieving-bernetta.ngrok-free.dev",
-  ],
+  output: "export",
+  trailingSlash: true,
+  basePath: isGithubPages ? repoName : "",
+  assetPrefix: isGithubPages ? repoName : "",
 
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
