3e960dc6930a5563f9d2eeecd9b707c58a946a34
jojoe260245 <jojoe260245@gmail.com>
2026-04-28 07:26:59 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "/ThungmakhamMiamiFood";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: isGithubPages ? repoName : "",
-  assetPrefix: isGithubPages ? repoName : "",
+  allowedDevOrigins: [
+    "latasha-unrelieving-bernetta.ngrok-free.dev",
+  ],
 
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
